89f90c8864c45442d5b8d09f7f52b493f533ba21
Hugo Trippaers <htrippaers@schubergphilis.com>
2026-07-02 09:18:38 +0200

    Fix compile warning

--- packet-mbus-dsmr6.c
+++ packet-mbus-dsmr6.c
@@ -390,7 +390,7 @@ proto_register_dsmr6(void)
 static bool
 dissect_dsmr6_heur(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
-    if (dissect_dsmr6(tvb, pinfo, tree, false) == 0) {
+    if (dissect_dsmr6(tvb, pinfo, tree, data) == 0) {
         // Not a valid MBus DSMR6 packet
         return false;
     }
